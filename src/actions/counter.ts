c579fa000255ec32afb9c8f71b6d610e4ff9de60
林湛杰 <linzhanjie@meican.com>
2020-10-20 17:53:54 +0800

    Initial commit

---
+++ src/actions/counter.ts
@@ -0,0 +1,42 @@
+import Taro from '@tarojs/taro'
+import {
+  ADD,
+  MINUS,
+  GETTHEME
+} from '../constants/counter'
+import { Dispatch } from 'redux'
+
+export const add = () => {
+  return {
+    type: ADD
+  }
+}
+export const minus = () => {
+  return {
+    type: MINUS
+  }
+}
+
+export const getTheme = () => {
+  return async (dispatch: Dispatch) => {
+    const res = await Taro.request({
+      url: 'http://127.0.0.1:9527/api/theme',
+      method: 'GET',
+    })
+    dispatch({
+      type: GETTHEME,
+      payload: {
+        theme: res.data
+      }
+    })
+  }
+}
+
+// 异步的action
+export function asyncAdd () {
+  return (dispatch: Dispatch) => {
+    setTimeout(() => {
+      dispatch(add())
+    }, 2000)
+  }
+}
---
+++ src/reducers/counter.ts
@@ -0,0 +1,33 @@
+import { ADD, MINUS, GETTHEME } from '../constants/counter'
+
+const INITIAL_STATE = {
+  num: 0,
+  theme: {
+    primary: '#fff',
+    linear: '#fff',
+    red: '#fff',
+    blue: '#fff',
+  }
+}
+
+export default function counter(state = INITIAL_STATE, action: { type: string; payload: any }) {
+  switch (action.type) {
+    case ADD:
+      return {
+        ...state,
+        num: state.num + 1
+      }
+    case MINUS:
+      return {
+        ...state,
+        num: state.num - 1
+      }
+    case GETTHEME:
+      return {
+        ...state,
+        theme: action.payload.theme
+      }
+    default:
+      return state
+  }
+}
